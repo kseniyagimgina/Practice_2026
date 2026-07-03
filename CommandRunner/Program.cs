0c0a7f9f8efa1c59a0182d2b6a5d95e54f5e038c
kseniyagimgina <kgimgina07@mail.ru>
2026-07-03 15:30:24 +0600

    feat: Metadata from lib

---
+++ CommandRunner/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+namespace CommandRunner
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string Dir = AppDomain.CurrentDomain.BaseDirectory;
+            string FilePath = Path.Combine(Dir, "FileSystemCommands.dll");
+            Assembly assembly = Assembly.LoadFrom(FilePath);
+
+            string dir = Path.Combine(Path.GetTempPath(), "Dir");
+            Directory.CreateDirectory(dir);
+            File.WriteAllText(Path.Combine(dir, "file1.txt"), "Hello World");
+            File.WriteAllText(Path.Combine(dir, "file2.doc"), "Document");
+            File.WriteAllText(Path.Combine(dir, "file3.txt"), "README");
+
+            Type size = assembly.GetType("FileSystemCommands.DirectorySizeCommand")!;
+            if (size != null)
+            {
+                object filesize= Activator.CreateInstance(size, dir)!;
+                dynamic command = filesize;
+                command.Execute(); 
+                Console.WriteLine($"Размер всех файлов составляет {command.Size} байт");
+            }
+            Type findmask = assembly.GetType("FileSystemCommands.FindFilesCommand")!;
+            if (findmask != null)
+            {
+                object findfile= Activator.CreateInstance(findmask, dir, "*.txt")!;
+                dynamic command = findfile;
+                command.Execute(); 
+                Console.WriteLine($"Количество файлов с маской {command.Mask} равно {command.Files.Count}:");
+                for (int i = 0; i < command.Files.Count; i++)
+                {
+                    Console.WriteLine(command.Files[i].Name);
+                }
+            }
+        }
+    }
+}
---
+++ task09tests/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System.Reflection;
+using FileSystemCommands;
+using task07;
+namespace task09tests;
+
+public class Console_Test
+{
+    [Fact]
+    public void GetMethodsFromDirectorySizeCommand()
+    {
+        Type type = typeof(DirectorySizeCommand);
+        MethodInfo[] methods = type.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+        Assert.Contains(methods, a => a.Name == "Execute");
+        Assert.Contains(methods, b => b.Name == "get_Size");
+        Assert.Contains(methods, c => c.Name == "get_Filepath");
+    }
+
+    [Fact]
+    public void GetMethodsFromFindFilesCommand() 
+    {
+        Type type = typeof(FindFilesCommand);
+        MethodInfo[] methods = type.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+        Assert.Contains(methods, a => a.Name == "Execute");
+        Assert.Contains(methods, b => b.Name == "get_Mask");
+        Assert.Contains(methods, c => c.Name == "get_Filepath");
+    }
+
+    [Fact]
+    public void GetConstructorsFromDirectorySizeCommandWithParameters()
+    {
+        Type type = typeof(DirectorySizeCommand);
+        ConstructorInfo[] constructors = type.GetConstructors(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        Assert.Single(constructors[0].GetParameters());
+        Assert.Equal("String", constructors[0].GetParameters()[0].ParameterType.Name);
+    }
+
+    [Fact]
+    public void GetConstructorsFromFindFilesCommandWithParameters()
+    {
+        Type type = typeof(FindFilesCommand);
+        ConstructorInfo[] constructors = type.GetConstructors(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        Assert.Equal(2, constructors[0].GetParameters().Length); 
+        Assert.Equal("String", constructors[0].GetParameters()[0].ParameterType.Name);
+        Assert.Equal("String", constructors[0].GetParameters()[1].ParameterType.Name);
+    }
+
+    [Fact]
+    public void GetAttributesFromDirectorySizeCommandWithParameters()
+    {
+        Type type = typeof(DirectorySizeCommand);
+        var displayNameAttr = type.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(displayNameAttr);
+        Assert.Equal("Команда определения размера каталога", displayNameAttr.DisplayName);
+        var versionAttr = type.GetCustomAttribute<VersionAttribute>();
+        Assert.NotNull(versionAttr);
+        Assert.Equal(1, versionAttr.Major);
+        Assert.Equal(0, versionAttr.Minor);
+    }
+
+    [Fact]
+    public void GetAttributesFromFindFilesCommand()
+    {
+        Type type = typeof(FindFilesCommand);
+        var displayNameAttr = type.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(displayNameAttr);
+        Assert.Equal("Команда поиска файлов по маске", displayNameAttr.DisplayName);
+        var versionAttr = type.GetCustomAttribute<VersionAttribute>();
+        Assert.NotNull(versionAttr);
+        Assert.Equal(1, versionAttr.Major);
+        Assert.Equal(0, versionAttr.Minor);
+    }
+}
