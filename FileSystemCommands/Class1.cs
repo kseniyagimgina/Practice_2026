27d8ad37a2e70fd0dd6c333d96d09141581159dc
kseniyagimgina <kgimgina07@mail.ru>
2026-07-03 15:30:24 +0600

    MetaData of Lib

---
+++ FileSystemCommands/Class1.cs
@@ -0,0 +1,57 @@
+﻿using CommandLib;
+using task07;
+namespace FileSystemCommands;
+
+[DisplayName("Определение размера каталога")]
+[Version(1,0)]
+public class DirectorySizeCommand: ICommand
+{
+    public string Filepath {get;}
+    public long Size {get; private set;}
+    public List<FileInfo> Files {get;} = new List<FileInfo>();
+    public DirectorySizeCommand(string filepath)
+    {
+        Filepath = filepath;
+    }
+    public void Execute()
+    {
+        Size = 0;
+        Files.Clear();
+        if (!Directory.Exists(Filepath))
+        {
+            return;
+        }
+        var dir = new DirectoryInfo(Filepath);
+        foreach (var file in dir.EnumerateFiles(".", SearchOption.AllDirectories))
+        {
+            Size += file.Length;
+            Files.Add(file);
+        }
+    }
+}
+
+[DisplayName("Нахождение файлов по маске")]
+[Version(1,0)]
+public class FindFilesCommand: ICommand
+{
+    public string Filepath {get;}
+    public string Mask {get;}
+    public List<FileInfo> Files {get;} = new List<FileInfo>();
+    public FindFilesCommand(string filepath, string mask)
+    {
+        Filepath = filepath;
+        Mask = mask;
+    }
+    public void Execute()
+    {
+        Files.Clear();
+        if (!Directory.Exists(Filepath))
+        {
+            return;
+        }
+       foreach (var file in Directory.GetFiles(Filepath, Mask, SearchOption.AllDirectories))
+        {
+        Files.Add(new FileInfo(file));
+        }
+    }
+}
---
+++ task09tests/UnitTest1.cs
@@ -0,0 +1,95 @@
+﻿using System.Reflection;
+using FileSystemCommands;
+using task07;
+using task09;
+namespace task09tests;
+
+public class Console_Test
+{
+    public class ConsoleOutputTest
+    {
+        public string ConsoleRun()
+        {
+            var output = new StringWriter();
+            Console.SetOut(output);
+            string path = Path.GetFullPath("../../../../FileSystemCommands/bin/Debug/net10.0/FileSystemCommands.dll");
+            string[] args = new string[] {path};
+            Program.Main(args);
+            return output.ToString();
+        }
+        [Fact]
+        public void MetaDataOfDirectorySizeCommand()
+        {
+            string output = ConsoleRun();
+            Assert.Contains("Библиотека: FileSystemCommands", output);
+
+            Assert.Contains("Класс DirectorySizeCommand:", output);
+
+            Assert.Contains("public String get_Filepath()", output);
+            Assert.Contains("public Int64 get_Size()", output);
+            Assert.Contains("private Void set_Size(Int64 value)", output);
+            Assert.Contains("public List`1 get_Files()", output);
+            Assert.Contains("public Void Execute()", output);
+
+            Assert.Contains("DisplayNameAttribute", output);
+            Assert.Contains("VersionAttribute", output);
+
+            Assert.Contains("public DirectorySizeCommand(String filepath)", output);
+
+            
+        }
+        [Fact]
+        public void MetaDataOfFindFilesCommand()
+        {
+            string output = ConsoleRun();
+            
+            Assert.Contains("Библиотека: FileSystemCommands", output);
+
+            Assert.Contains("Класс FindFilesCommand:", output);
+
+            Assert.Contains("public String get_Filepath()", output);
+            Assert.Contains("public String get_Mask()", output);
+            Assert.Contains("public List`1 get_Files()", output);
+            Assert.Contains("public Void Execute()", output);
+
+            Assert.Contains("DisplayNameAttribute", output);
+            Assert.Contains("VersionAttribute", output);
+
+
+            Assert.Contains("public FindFilesCommand(String filepath, String mask)", output);
+
+        }
+        [Fact]
+        public void MetaDataOfAttributeClasses()
+        {
+            string output = ConsoleRun();
+
+            Assert.Contains("Библиотека: FileSystemCommands", output);
+
+            Assert.Contains("Класс DisplayNameAttribute:", output);
+            Assert.Contains("public String get_DisplayName()", output);
+            Assert.Contains("public DisplayNameAttribute(String displayname)", output);
+
+            Assert.Contains("AttributeUsageAttribute", output);
+
+            Assert.Contains("Класс VersionAttribute:", output);
+            Assert.Contains("public Int32 get_Major()", output);
+            Assert.Contains("public VersionAttribute(Int32 major, Int32 minor)", output);
+        }
+        [Fact]
+        public void MetaDataOfSampleClassAndReflectionHelper()
+        {
+            string output = ConsoleRun();
+
+            Assert.Contains("Библиотека: FileSystemCommands", output);
+
+            Assert.Contains("Класс SampleClass:", output);
+            Assert.Contains("public Void TestMethod()", output);
+            Assert.Contains("public Int32 get_Number()", output);
+            Assert.Contains("public SampleClass()", output);
+
+            Assert.Contains("Класс ReflectionHelper:", output);
+            Assert.Contains("public Void PrintTypeInfo(Type type)", output);
+        }
+    }
+}
