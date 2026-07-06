bb53761551da79b80b8c9e16b3bf7a87a7ca7d99
kseniyagimgina <kgimgina07@mail.ru>
2026-07-06 14:05:06 +0600

    refactor: splite file into 3

---
+++ task04/Cruiser.cs
@@ -0,0 +1,22 @@
+﻿namespace task04;
+
+public class Cruiser: ISpaceship
+{
+    public void MoveForward()
+    {
+        Coordinate += Speed;
+    }
+    public void Rotate(int angle)
+    {
+        Angle = (Angle + angle) % 360;
+    }
+    public void Fire()
+    {
+        Shoot++;
+    }    
+    public int Speed {get;} = 50;
+    public int FirePower {get;} = 100;
+    public int Coordinate {get; private set;} = 0;
+    public int Angle {get; private set;} = 0;
+    public int Shoot {get; private set;} = 0;
+}
